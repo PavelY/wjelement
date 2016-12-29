5014de96c633c18dd7390b250b1c4d27dff29ead
pavel yurchenko <yurchenko.pavel@gmail.com>
2016-12-29 15:21:30 +0300

    iOS framework project added

---
+++ wjelement_ios/wjelement_ios/wjelement_ios.h
@@ -0,0 +1,19 @@
+//
+//  wjelement_ios.h
+//  wjelement_ios
+//
+//  Created by Pavel Yurchenko on 12/29/16.
+//  Copyright © 2016 Tipit Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for wjelement_ios.
+FOUNDATION_EXPORT double wjelement_iosVersionNumber;
+
+//! Project version string for wjelement_ios.
+FOUNDATION_EXPORT const unsigned char wjelement_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <wjelement_ios/PublicHeader.h>
+
+
