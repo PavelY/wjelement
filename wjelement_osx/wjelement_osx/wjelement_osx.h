bbaad12dd28be0529f7b61e50cbb0c25e0001d3a
pavel yurchenko <yurchenko.pavel@gmail.com>
2016-12-29 14:35:46 +0300

    OSX framework project added

---
+++ wjelement_osx/wjelement_osx/wjelement_osx.h
@@ -0,0 +1,19 @@
+//
+//  wjelement_osx.h
+//  wjelement_osx
+//
+//  Created by Pavel Yurchenko on 12/29/16.
+//  Copyright © 2016 Tipit Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for wjelement_osx.
+FOUNDATION_EXPORT double wjelement_osxVersionNumber;
+
+//! Project version string for wjelement_osx.
+FOUNDATION_EXPORT const unsigned char wjelement_osxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <wjelement_osx/PublicHeader.h>
+
+
---
+++ wjelement_osx/wjelement_osxTests/wjelement_osxTests.m
@@ -0,0 +1,39 @@
+//
+//  wjelement_osxTests.m
+//  wjelement_osxTests
+//
+//  Created by Pavel Yurchenko on 12/29/16.
+//  Copyright © 2016 Tipit Ltd. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface wjelement_osxTests : XCTestCase
+
+@end
+
+@implementation wjelement_osxTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
